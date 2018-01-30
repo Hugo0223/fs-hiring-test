5f444ffc17f492bbb3269af1879856a881841dd6
Hugo Guitton <guitton.h@gmail.com>
2018-01-30 15:48:17 +0100

    Revenues calculation

--- app/controllers/motels_controller.rb
+++ app/controllers/motels_controller.rb
@@ -4,61 +4,14 @@ class MotelsController < ApplicationController
   # GET /motels
   # GET /motels.json
   def index
-    @motels = Motel.all
+    @motels = current_user.motels
+    @revenues_motels = revenues_resources(@motels)
   end
 
   # GET /motels/1
   # GET /motels/1.json
   def show
-  end
-
-  # GET /motels/new
-  def new
-    @motel = Motel.new
-  end
-
-  # GET /motels/1/edit
-  def edit
-  end
-
-  # POST /motels
-  # POST /motels.json
-  def create
-    @motel = Motel.new(motel_params)
-
-    respond_to do |format|
-      if @motel.save
-        format.html { redirect_to @motel, notice: 'Motel was successfully created.' }
-        format.json { render :show, status: :created, location: @motel }
-      else
-        format.html { render :new }
-        format.json { render json: @motel.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /motels/1
-  # PATCH/PUT /motels/1.json
-  def update
-    respond_to do |format|
-      if @motel.update(motel_params)
-        format.html { redirect_to @motel, notice: 'Motel was successfully updated.' }
-        format.json { render :show, status: :ok, location: @motel }
-      else
-        format.html { render :edit }
-        format.json { render json: @motel.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /motels/1
-  # DELETE /motels/1.json
-  def destroy
-    @motel.destroy
-    respond_to do |format|
-      format.html { redirect_to motels_url, notice: 'Motel was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    # Show Motel => Motel infos + all rooms in motel with revenue
   end
 
   private
