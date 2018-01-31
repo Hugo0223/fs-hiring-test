65490ec6ae02f4e7e15f17ec2d4e78435156e25d
Hugo Guitton <guitton.h@gmail.com>
2018-01-31 20:20:23 +0100

    Dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < ActionController::Base
 	end
 
   def after_sign_in_path_for(resource)
-  	bookings_path
+  	home_path
 	end
 
 	def after_sign_out_path_for(resource)
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,31 @@
 class HomeController < ApplicationController
-
+	
+	# Dashboard 
 	def index
-		
-	end
+    	@bookings = policy_scope(Booking).order(created_at: :desc)
+        @global_revenues = revenues_bookings(@bookings)
+        @bookings_per_month = @bookings.all.group_by { |m| m.created_at.beginning_of_month }
+        @bookings_per_motel = @bookings.all.group_by { |m| m.motel }
 
+        ## Table "Revenues per months"
+        @monthly_datas = []
+        @bookings_per_month.each do |month|
+        	# Month[0] : name of the month
+        	# Month[1] : array of bookings during that month
+        	@monthly_datas << {
+        		"name" => month[0].strftime("%b %Y"),
+        		"bookings" => month[1].count,
+        		"revenues" =>revenues_bookings(month[1])
+        	}
+        end
+        ## Table "Revenues per motel"
+        @motels_datas = []
+        @bookings_per_motel.each do |motel|
+    			@motels_datas << {
+        		"name" => motel[0].name,
+        		"bookings" => motel[1].count,
+        		"revenues" =>revenues_bookings(motel[1])
+        	}
+        end
+	end
 end
